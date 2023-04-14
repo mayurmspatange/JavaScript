abaf3f2e9fa63490e9b0bb95f9f7990e06623f79
anita <anita.khamitkar@gmail.com>
2023-04-14 09:17:03 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,32 @@
+// variable declaratoin 
+var a;
+var b;
+var c;
+
+// variable initilization 
+a = 10;
+b = 11;
+c = a + b;
+a = 40;
+
+console.log("value of c: "+c);
+
+
+// variable   declaratoin and initilization 
+let x = 10;
+let y = 20;
+let z = x + y;
+
+
+console.log("Addition of x  + y = ",z);
+
+ t = 10;
+ u = 20;
+ o = t + u;
+console.log("Addition of t  + u = ", o);
+
+
+// const variable
+
+const PI = 3.14;
+// PI = 56;
