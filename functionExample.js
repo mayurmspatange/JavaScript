baad5369c5eaac6f010ed7424bb65b6a3d181673
anita <anita.khamitkar@gmail.com>
2023-05-04 09:01:30 +0530

    function types

--- functionExample.js
+++ functionExample.js
@@ -16,3 +16,32 @@ result=  areaOfRectangle(45,2);
 console.log("area of Rectangle :", result);
 
 
+
+
+
+// no parameters and no return value
+function fahrenheitToCelsius(){
+    let celsius = (23-32) * (5/9)
+    console.log("celsius", celsius)
+}
+
+fahrenheitToCelsius();
+
+// withParameters no return value
+
+function fahrenheitToCelsiusWithParametes(fahr){
+    let celsius = (fahr-32) * (5/9)
+    console.log("celsius of fahrenheit",fahr," is : " , celsius)
+}
+
+fahrenheitToCelsiusWithParametes(50)
+
+// withParameters wiht return value
+
+function fahrenheitToCelsiusWithParametesWihtRetun(fahr){
+    let celsius = (fahr-32) * (5/9)
+    return celsius;
+}
+
+let res = fahrenheitToCelsiusWithParametesWihtRetun(60)
+console.log("celsius of fahrenheit",60," is : " , res)
