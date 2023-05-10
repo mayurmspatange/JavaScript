75182094108adca675ba26b1f22ea37e0efcfb34
anita <anita.khamitkar@gmail.com>
2023-05-10 10:55:36 +0530

    array

---
+++ Array.js
@@ -0,0 +1,69 @@
+var carCompany1 = "BMW";  // 2 byte
+            //    0123
+var carCompany2 = "TATA";   // 2 byte 
+
+
+// let carCom = ["BMW", "TATA", "jeep", "mahindra" , 231, new Date()];
+
+// 1 st way
+            //  0       1       2       3
+let carCom = ["BMW", "TATA", "jeep", "mahindra"];
+
+
+// | BMW | TATA | jeep | mahindra | 
+
+// console.log(carCompany1)
+// console.log(carCompany2)
+// console.log(carCom)
+
+console.log(carCom)
+
+carCom[4] = "volvo"
+
+console.log(carCom)
+
+carCom[5] = "tesla"
+
+console.log(carCom)
+            // 0     1        2        3       4
+let name = ["neha","mayur","deepak","ganesh","krishna"]; 
+console.log(name);
+
+name[2] = "shubham";
+
+console.log(name);
+
+name[5] = "pavan";
+
+console.log(name);
+
+name[7] = "sunita";
+
+console.log(name);
+            //  0     1      2        3         4
+var animal = ["dog","can","horse","elephant","zebra"];
+console.log(animal);
+
+animal[0] = "lion";
+console.log(animal);
+
+animal[4] = "tiger";
+console.log(animal);
+
+animal[5] = "kangaroo";
+console.log(animal);
+            //      0     1     2        3        4       5   
+let furniture = ["sofa","bed","chair","dresser","desk","table"];
+
+console.log(furniture);
+
+furniture[0] = "bench";
+console.log(furniture);
+
+furniture[4] = "bookself";
+console.log(furniture);
+
+furniture[6] = "coffee table";
+console.log(furniture);
+
+
--- numbers.js
+++ numbers.js
@@ -39,3 +39,59 @@ console.log(c1.toFixed(0));
 console.log(c1.toFixed(2));
 console.log(c1.toFixed(4));
 console.log(c1.toFixed(6));
+
+
+
+let no = 123;
+
+
+console.log("before Converted value ", typeof no);
+
+let str = no.toString();
+console.log("Converted value ", typeof str);
+
+(123).toString();
+(100 + 23).toString();
+
+
+// Number: 
+    // interger : -1 ,-2, -3 ,-4, -5, -6 0  1 ,2, 3 ,4, 5, 6.. . . . 
+    // float value: 1.234, 21.213 2.123
+
+
+let number = 10.10;
+console.log(parseInt(number))
+// toString;
+
+let intNumber =  "56";
+console.log("intNumber:", typeof intNumber);
+console.log(typeof parseFloat(intNumber))
+
+
+console.log(Number(true));
+
+// Number(true) + "<br>" +
+// Number(false) + "<br>" +
+// Number("10") + "<br>" + 
+// Number("  10") + "<br>" +
+// Number("10  ") + "<br>" +
+// Number(" 10  ") + "<br>" +
+// Number("10.33") + "<br>" + 
+// Number("10,33") + "<br>" +
+// Number("10 33") + "<br>" +
+// Number("John");
+
+// Number.parseInt("-10");
+// Number.parseInt("-10.33");
+// Number.parseInt("10");
+// Number.parseInt("10.33");
+// Number.parseInt("10 20 30");
+// Number.parseInt("10 years");
+// Number.parseInt("years 10");
+
+
+console.log("MAX_VALUE in jS", Number.MAX_VALUE);
+console.log("MIN_VALUE in jS", Number.MIN_VALUE);
+
+console.log("MIN_SAFE_INTEGER in jS", Number.MIN_SAFE_INTEGER);
+console.log("MAX_SAFE_INTEGER in jS", Number.MAX_SAFE_INTEGER);
