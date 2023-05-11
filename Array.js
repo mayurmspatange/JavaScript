6e886e301c914e12e821c561627009e1a02243bb
anita <anita.khamitkar@gmail.com>
2023-05-11 09:10:02 +0530

    Array methods

--- Array.js
+++ Array.js
@@ -25,21 +25,26 @@ console.log(carCom)
 carCom[5] = "tesla"
 
 console.log(carCom)
+
             // 0     1        2        3       4
 let name = ["neha","mayur","deepak","ganesh","krishna"]; 
 console.log(name);
 
 name[2] = "shubham";
 
-console.log(name);
+console.log("after name[2] replced",name);
 
 name[5] = "pavan";
 
-console.log(name);
+console.log("after adding a new element at name[5]",name);
 
 name[7] = "sunita";
 
-console.log(name);
+console.log("after adding a new element at name[7]",name);
+
+
+
+
             //  0     1      2        3         4
 var animal = ["dog","can","horse","elephant","zebra"];
 console.log(animal);
@@ -52,6 +57,8 @@ console.log(animal);
 
 animal[5] = "kangaroo";
 console.log(animal);
+
+
             //      0     1     2        3        4       5   
 let furniture = ["sofa","bed","chair","dresser","desk","table"];
 
@@ -65,5 +72,85 @@ console.log(furniture);
 
 furniture[6] = "coffee table";
 console.log(furniture);
+
+
+
+
+// carCom name animal furniture  rollNumbers
+
+console.log("CarCom array",carCom);
+
+
+let lenghtofarr = carCom.length;
+console.log("Length of car array",lenghtofarr);
+
+let carSorted = carCom.sort();
+console.log("Car array after sorting:", carSorted);
+
+let rollNumbers= [67,87,55,33,45,66,1];
+console.log("rollNumbers", rollNumbers);
+console.log("after sorting rollNumberArray",rollNumbers.sort());
+
+
+
+
+console.log("name array",name);
+console.log("Length of name array: ",name.length);
+console.log("sort of name array: ",name.sort());
+
+
+
+console.log("animal array",animal);
+console.log("Length of animal array: ",animal.length);
+console.log("sort of animal array: ",animal.sort());
+
+
+console.log("furniture array",furniture);
+console.log("Length of furniture array: ",furniture.length);
+console.log("sort of furniture array: ",furniture.sort());
+
+let arrtostr = furniture.toString();
+console.log("type of furniture : ", typeof furniture);
+console.log("type of arrtostr : ", typeof arrtostr);
+
+
+let recoreds = furniture.push("tv unit");
+let recoreds1 = furniture.push("tea pot unit");
+let newfurniture = furniture.concat("door");
+
+console.log("after push furniture : ",recoreds1, furniture);
+console.log("after concat newfurniture : ", newfurniture);
+
+let ele = furniture.pop()
+console.log("after pop furniture : ",ele, furniture);
+
+let ele1 = furniture.pop()
+console.log("after pop furniture : ",ele1, furniture);
+
+delete furniture[2];
+console.log("after delete furniture[2] furniture : ", furniture);
+
+
+
+
+// Merging (Concatenating) Arrays
+
+let furnitureAndAnimal = furniture.concat(animal);
+console.log("after Concatenating furniture and animal : ", furnitureAndAnimal);
+
+
+let furnitureAndAnimalAndCarCom = furniture.concat(animal,carCom);
+console.log("after Concatenating furniture and animal and carCom array : ", furnitureAndAnimalAndCarCom);
+
+
+
+
+
+
+
+
+
+
+
 
 
