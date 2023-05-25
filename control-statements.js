e827ca8ec5570c31b7eb0718f6c27e5b2737dec0
anita <anita.khamitkar@gmail.com>
2023-05-25 08:52:39 +0530

    control-statement

---
+++ control-statements.js
@@ -0,0 +1,29 @@
+for (let index = 1; index <= 10; index++) {
+    console.log("in loop");
+    if(index==5){
+        console.log("in condition");
+        break;
+        console.log("");
+        console.log("");
+    }
+    console.log("i = ", index);    
+}
+
+console.log("");
+console.log("");
+
+
+for (let index = 1; index <= 10; index++) {
+    console.log("in loop");
+    if(index==5){
+        console.log("in condition");
+        continue;
+        console.log("");
+        console.log("");
+    }
+    console.log("i = ", index);    
+}
+
+console.log("end");
+
+1234_6
